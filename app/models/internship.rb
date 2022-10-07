1ec3d78b85b88cc32e17b2c140681ddf17278e1d
Michael Barnard <michaeljbarnard100@gmail.com>
2022-10-07 14:11:54 +0100

    doeoon

--- app/models/internship.rb
+++ app/models/internship.rb
@@ -1,11 +1,11 @@
 class Internship < ApplicationRecord
-    geocoded_by :location
-    after_validation :geocode, if: :will_save_change_to_location?
-    validates :company, presence: true
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
+    # validates :company, presence: true
+    validates :title, presence: true
     validates :address, presence: true
-    validates :paid, presence: true
-    validates :dates, presence: true
-    validates :industry, presence: true
-    validates :description, presence: true
-    validates :description, presence: true
+    # validates :paid, presence: true
+    # validates :dates, presence: true
+    # validates :industry, presence: true
+    # validates :description, presence: true
 end
